03e5cd05207e8076ba8ac67f9b6e2028efad5987
Jacob Brugh <jacobpbrugh@gmail.com>
2024-04-15 00:07:18 -0400

    switch / placement

--- src/custom_config.h
+++ src/custom_config.h
@@ -15,12 +15,12 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_LAYER_NUM \
 U_NP,              7,                 8,                 9,                 U_NP,              U_NA,              U_DF(U_BASE),      U_DF(U_EXTRA),     U_DF(U_TAP),       U_NA,              \
 ;,                 1,                 2,                 3,                 =,                 U_NA,              sft,               ctl,               alt,               met,               \
-U_NP,              4,                 5,                 6,                 \\,                U_NA,              U_DF(U_NUM),       U_DF(U_NAV),       ralt,              U_NA,              \
+U_NP,              4,                 5,                 6,                 U_NP,              U_NA,              U_DF(U_NUM),       U_DF(U_NAV),       ralt,              U_NA,              \
 U_NP,              U_NP,              `,                 0,                 -,                 U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 U_NP,              @,                 #,                 $,                 U_NP,              U_NA,              U_DF(U_BASE),      U_DF(U_EXTRA),     U_DF(U_TAP),       U_NA,              \
-[,                 ],                 U_LPRN,            U_RPRN,            U_NP,              U_NA,              sft,               ctl,               alt,               met,               \
+[,                 ],                 U_LPRN,            U_RPRN,            \\,                U_NA,              sft,               ctl,               alt,               met,               \
 %,                 ^,                 &,                 *,                 U_NP,              U_NA,              U_DF(U_SYM),       U_DF(U_MOUSE),     ralt,              U_NA,              \
 U_NP,              U_NP,              !,                 :,                 U_NP,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
